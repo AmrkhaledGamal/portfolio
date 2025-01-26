47214f21182d1948d184ec3c1a312c5a9f7fe8d7
AmrkhaledGamal <amrkhaled45555@gmail.com>
2025-01-26 05:25:59 +0200

    i create a portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServiceComponent } from './components/service/service.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectComponent } from './components/project/project.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NavbarComponent,
+    HomeComponent,
+    ServiceComponent,
+    SkillsComponent,
+    AboutComponent,
+    ProjectComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
+
+  scrollUp: boolean = false;
+
+  constructor() {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 400) {
+        this.scrollUp = true;
+      } else {
+        this.scrollUp = false;
+      }
+    });
+  }
+
+  scrollTop(): void {
+    window.scroll({
+      top: 0,
+    });
+  }
 }
