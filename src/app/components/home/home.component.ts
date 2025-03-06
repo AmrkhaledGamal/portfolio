f12a83f55be4403680f04d961518e116b54d8df3
AmrkhaledGamal <amrkhaled45555@gmail.com>
2025-03-06 02:13:02 +0200

    spinner download

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,8 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  loading = false;
 
+  downloadCV() {
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+    }, 500);
+  }
 }
